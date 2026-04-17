b7f3704fcb849e2ad6d411924360e04ee761397e
aleksgogic <aleks03gogicaleks@outlook.com>
2026-04-17 14:41:49 +0200

    Dodana kontrolerja za ratings in reports

--- api/routes/ratings.js
+++ api/routes/ratings.js
@@ -1,15 +1,24 @@
-import { Router } from "express";
-import ratingsCtrl from "../controllers/ratings.js";
+import { Router } from 'express';
+import ratingsCtrl from '../controllers/ratings.js';
+import { protect, restrictTo } from '../middleware/auth.js';
 
 const ratingsRouter = Router();
 
 // GET /api/ratings
-ratingsRouter.get("/", ratingsCtrl.getAllRatings);
+ratingsRouter.get('/', protect, restrictTo('admin'), ratingsCtrl.getAllRatings);
 
 // POST /api/ratings
-ratingsRouter.post("/", ratingsCtrl.createRating);
+ratingsRouter.post('/', protect, restrictTo('admin', 'user'), ratingsCtrl.createRating);
+
+// PUT /api/ratings/:ratingId
+ratingsRouter.put('/:ratingId', protect, restrictTo('admin', 'user'), ratingsCtrl.updateRating);
 
 // DELETE /api/ratings/:ratingId
-ratingsRouter.delete("/:ratingId", ratingsCtrl.deleteRating);
+ratingsRouter.delete(
+	'/:ratingId',
+	protect,
+	restrictTo('admin', 'user'),
+	ratingsCtrl.deleteRating,
+);
 
 export default ratingsRouter;
